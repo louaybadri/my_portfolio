9340ee1cfa0d805d25533b2f45d717d0fea349a7
Louay Badri <louay.badri@insat.ucar.tn>
2022-11-26 22:08:52 +0100

    recommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from "./Components/Big Components/NavBar/NavBar";
+
+
+function App
+() {
+
+    return (
+        <div className="full-screen">
+            <NavBar/>
+            <h4>{"<h1>This is</h1>"}</h4>
+        </div>
+    );
+}
+
+export default App;
